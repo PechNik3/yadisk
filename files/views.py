de3140a90453758bfee0bd56cb6eb767f105ecee
PechNik3 <nik_t.n.t@inbox.ru>
2025-05-17 14:55:31 +1000

    просмотр и скачивание содержимого папки Яндекс.Диска

--- files/views.py
+++ files/views.py
@@ -1,3 +1,28 @@
+import requests
 from django.shortcuts import render
+from typing import Optional
 
-# Create your views here.
+YANDEX_API_URL = 'https://cloud-api.yandex.net/v1/disk/public/resources'
+
+
+def home(request):
+    public_key = request.GET.get('public_key')
+    files: Optional[list] = None
+
+    if public_key:
+        response = requests.get(YANDEX_API_URL, params={
+            'public_key': public_key,
+            'limit': 1000,
+        })
+
+        if response.status_code == 200:
+            data = response.json()
+            embedded = data.get('_embedded', {})
+            items = embedded.get('items', [])
+            files = [{
+                'name': item['name'],
+                'type': item['type'],
+                'file': item.get('file')
+            } for item in items]
+
+    return render(request, 'files/home.html', {'files': files})
